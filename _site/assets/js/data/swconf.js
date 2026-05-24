09c8e139447af0640e192f8d5be64c816a7bed36
Marat Saidov <dev@msaidov.com>
2026-05-24 17:39:20 +0200

    fixed

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1753818802',resources: [
+    cacheName: 'chirpy-1779637145',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
